e0e374e8ec41a2bfd069fc9deaa624ac1438cf2f
Reinhoudt <j.e.reinhoudt@student.tue.nl>
2023-05-03 11:38:04 +0200

    variable name changes

--- isi_channel_v3.m
+++ isi_channel_v3.m
@@ -9,24 +9,25 @@ tic
 sequenceLength = 1e5;
 memory = 2;
 symbols = [-1 1];
-symbolAmount = length(symbols);
-iterations = 100;
+M = length(symbols);
+iterations = 10;
 SNRDB = -4:2:16; %SNR in dB
 SNR=10.^(SNRDB/10); %linear SNR 
 avBER = 0;
 avBERref = 0;
 avBERSxS = 0;
 energy = 1;
+noStates = M^memory;
 
 
 %% Generate states
 %initialize states
-states = zeros(symbolAmount^memory,memory);
+states = zeros(noStates,memory);
 for i=1:memory
     temp = 1;
-    for j=1:(symbolAmount^(i-1))
-        for k=1:symbolAmount
-            for m=1:(symbolAmount^(memory-i))
+    for j=1:(M^(i-1))
+        for k=1:M
+            for m=1:(M^(memory-i))
                 states(temp,i) = symbols(k);
                 temp = temp + 1;
             end
@@ -35,107 +36,109 @@ for i=1:memory
 end
 
 
-for iter = 1:iterations
-    disp(iter);
+   
     
-        %% Get random sequence
-        bitSequence = randi([0 1], sequenceLength, 1);
-        antipodalSequence = bitToAntipodal(bitSequence, energy);
-        channelMem = [1,normrnd(0,.1,1,memory)];
-        %channelMem = [1,0.4,-0.2];
-        ISISequenceNoNoise = conv(channelMem, antipodalSequence);
+%% Get random sequence
+bitSequence = randi([0 1], sequenceLength, 1);
+TxSequence = bitToAntipodal(bitSequence, energy);
+%channelCoef = [1,normrnd(0,.3,1,memory)];
+channelCoef = [1,0.4,-0.2];
+ISISequence = conv(channelCoef, TxSequence);
 
-        BER = zeros(1,length(SNRDB));
-        BERref = zeros(1,length(SNRDB));
-        BERSxS = zeros(1,length(SNRDB));
+BER = zeros(1,length(SNRDB));
+BERref = zeros(1,length(SNRDB));
+BERSxS = zeros(1,length(SNRDB));
 
-    for asdf = 1:length(SNRDB)
-        ISISequence = addAWGN(ISISequenceNoNoise, SNRDB(asdf));
-        %ISISequence = [1, -1.2,  0.5, -1.5, -0.2, 1, 0.8, 0.9];
-        %% Viterbi
-        %initialize trellis
-        trellis = zeros(symbolAmount^memory,length(ISISequence));
-        %trellis(:,1) = [2.4;-3.2;5.6;3.2];
-        path = zeros(symbolAmount^memory,length(ISISequence)-1);
-        pathMetrics = zeros(1,symbolAmount);
+for asdf = 1:length(SNRDB)
+    disp(asdf);
+    RxSequence = addAWGN(ISISequence, SNRDB(asdf));
+    %ISISequence = [1, -1.2,  0.5, -1.5, -0.2, 1, 0.8, 0.9];
+    %% Viterbi
+    %initialize trellis
+    trellis = zeros(noStates,length(RxSequence));
+    %trellis(:,1) = [2.4;-3.2;5.6;3.2];
+    path = zeros(noStates,length(RxSequence)-1);
+    pathMetrics = zeros(1,M);
 
-        %for loop over length of sequence
-        for i = 1:length(ISISequence)
-            %for loop over number of states
-            for j = 1:symbolAmount^memory
-                x = 1;
-                for m = 1:symbolAmount^memory
-                    %for loop over all possible paths from/to a state
-                    if(states(m,1:size(states,2)-1) == states(j,2:size(states,2)))
-                    temp1 = -2*states(j,1)*ISISequence(i);
-                    temp3 = states(j,1)^2;
-                    temp2 = 0;
-                        for k = 1:symbolAmount
-                            temp2 = states(m,k)*channelMem(k+1) + temp2;
-                        end
-                    temp2 = 2*states(j,1)*temp2;
-                    total = temp1+temp2+temp3;
-                    optie(x,:) = [trellis(m,i)+total,m];
-                    x = x+1;
+    %for loop over length of sequence
+    for i = 1:length(RxSequence)
+        %for loop over number of states
+        for j = 1:noStates
+            x = 1; 
+            for m = 1:noStates
+                %for loop over all possible paths from/to a state
+                if(states(m,1:size(states,2)-1) == states(j,2:size(states,2)))
+                temp1 = -2*states(j,1)*RxSequence(i);
+                temp3 = states(j,1)^2;
+                temp2 = 0;
+                    for k = 1:M
+                        temp2 = states(m,k)*channelCoef(k+1) + temp2;
                     end
+                temp2 = 2*states(j,1)*temp2;
+                total = temp1+temp2+temp3;
+                optie(x,:) = [trellis(m,i)+total,m];
+                x = x+1;
                 end
-                [~,b] = min(optie);
-                trellis(j,i+1) = optie(b(1),1);
-                path(j,i) = optie(b(1),2);
             end
+            [~,b] = min(optie);
+            trellis(j,i+1) = optie(b(1),1);
+            path(j,i) = optie(b(1),2);
         end
+    end
+% end of trellis processing 
 
-        k = zeros(1,length(path)+1);
-        test = length(path)+1;
-        for i = 1:test
-            if (i == 1)
-                [~,k(test)] = min(trellis(:,length(trellis)));
-            else
-                k(test-i+1) = path(k(test-i+2),length(path)+1-i+1);
+% Final sequence selection 
+    k = zeros(1,length(path)+1);
+    test = length(path)+1;
+    for i = 1:test
+        if (i == 1)
+            [~,k(test)] = min(trellis(:,length(trellis)));
+        else
+            k(test-i+1) = path(k(test-i+2),length(path)+1-i+1);
+        end
+    end
+
+    % decoding the Rx
+    decodedSyms = zeros(length(k)+1,1);
+    for i = 1:length(k)
+        if (i == 1)
+            for j = 1:memory
+                decodedSyms(j) = states(k(i), memory-j+1);
             end
+        else
+            decodedSyms(i+memory-1) = states(k(i),1);
         end
-
-        decodedBits = zeros(length(k)+1,1);
-        for i = 1:length(k)
-            if (i == 1)
-                for j = 1:memory
-                    decodedBits(j) = states(k(i), memory-j+1);
-                end
-            else
-                decodedBits(i+memory-1) = states(k(i),1);
-            end
-        end
-
-        recievedBits = antipodalToBit(decodedBits);
-        testing = recievedBits(memory+1:length(recievedBits)-2);
-        
-        reference = addAWGN(antipodalSequence, SNRDB(asdf));
-        referenceRecieved = estimateSignal(reference, energy);
-        referenceDecoded = antipodalToBit(referenceRecieved);
-        
-        SxSRecieved = estimateSignal(ISISequence, energy);
-        SxSDecoded = antipodalToBit(SxSRecieved);
-        
-        BER(asdf) = nnz(bitSequence - testing)/length(bitSequence);
-        BERref(asdf) = nnz(bitSequence - referenceDecoded)/length(bitSequence);
-        BERSxS(asdf) = nnz(bitSequence - SxSDecoded(1:length(bitSequence)))/length(bitSequence);
     end
     
-    avBER = avBER + BER;
-    avBERref = avBERref + BERref;
-    avBERSxS = avBERSxS + BERSxS;
-end
+    %aligning bits
+    recievedBits = antipodalToBit(decodedSyms);
+    alignedBitSeq = recievedBits(memory+1:length(recievedBits)-2);
 
-avBER = avBER / iterations;
-avBERref = avBERref/iterations;
-avBERSxS = avBERSxS/iterations;
+
+    % Comparison with ISI-free AWGN performance 
+    reference = addAWGN(TxSequence, SNRDB(asdf));
+    referenceRecieved = estimateSignal(reference, energy);
+    referenceDecoded = antipodalToBit(referenceRecieved);
+    
+    %Comparison with SxS 
+    SxSRecieved = estimateSignal(RxSequence, energy);
+    SxSDecoded = antipodalToBit(SxSRecieved);
+
+    BER(asdf) = sum(bitSequence + alignedBitSeq)/length(bitSequence);
+    BERref(asdf) = sum(bitSequence + referenceDecoded)/length(bitSequence);
+    BERSxS(asdf) = sum(bitSequence + SxSDecoded(1:length(bitSequence)))/length(bitSequence);
+
+%     BER(asdf) = sum(bitSequence ~= alignedBitSeq)/length(bitSequence);
+%     BERref(asdf) = sum(bitSequence ~= referenceDecoded)/length(bitSequence);
+%     BERSxS(asdf) = sum(bitSequence ~= SxSDecoded(1:length(bitSequence)))/length(bitSequence);
+end
 
 %% Plot
 figure
-semilogy(SNRDB,avBER,'-o');
+semilogy(SNRDB,BER,'-o');
 hold on;
-semilogy(SNRDB,avBERref,'-+');
-semilogy(SNRDB,avBERSxS,'-x')
+semilogy(SNRDB,BERref,'-+');
+semilogy(SNRDB,BERSxS,'-x')
 PE2=qfunc(sqrt(2*SNR));     %theoretical BER qfunc(sqrt(2Eb/N0)) Eb/N0 is linear SNR
 semilogy(SNRDB,PE2,'-');
 axis([-4 20 10^-6 10^0])
